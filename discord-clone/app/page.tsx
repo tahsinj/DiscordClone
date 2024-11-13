fa96c0994eed650041343876d38777fd56ff61e8
TanishqM1 <Tanishqmehta2004@gmail.com>
2024-11-12 23:10:30 -0800

    added POST api call

--- discord-clone/app/page.tsx
+++ discord-clone/app/page.tsx
@@ -2,8 +2,9 @@
 
 import Image from "next/image";
 import { User } from "stream-chat";
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { LoadingIndicator } from "stream-chat-react";
+import { useClerk } from "@clerk/nextjs";
 
 type HomeState = {
   apiKey: string;
@@ -14,6 +15,26 @@ type HomeState = {
 
 export default function Home() { 
   const [homeState, setHomeState] = useState<HomeState | undefined>();
+  const {user: clerkUser} = useClerk()
+
+  //register user function
+  const registerUser = useCallback(async function RegiserUser() {
+    const userId = clerkUser?.id;
+    const mail = clerkUser?.primaryEmailAddress?.emailAddress;
+    if (userId && mail){
+      const response = await fetch('api/register-user', {
+        method:'POST',
+        headers:{
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({userId: userId, email: mail}),
+
+      })
+      const responseBody = response.json();
+      return responseBody
+    }
+    
+  },[])
   
   // LoadingIndicator is displayed until we have a homestate to display.
   if (!homeState){return <LoadingIndicator/>};
