9e7a87851a07c905e3d3cdb958a6973f3a6efa36
Raad2004 <raad.sask@gmail.com>
2024-11-16 22:12:05 -0800

    created chat layout state

---
+++ discord-clone/components/myChat.tsx
@@ -0,0 +1,52 @@
+import { useClient } from '@/hooks/useClient';
+import { User } from 'stream-chat';
+import {
+  Chat,
+  Channel,
+  ChannelList,
+  ChannelHeader,
+  MessageList,
+  MessageInput,
+  Thread,
+  Window,
+} from 'stream-chat-react';
+
+export default function MyChat({
+    apiKey,
+    user,
+    token,
+  }: {
+    apiKey: string;
+    user: User;
+    token: string;
+  }) {
+    const chatClient = useClient({
+      apiKey,
+      user,
+      tokenOrProvider: token,
+    });
+
+    if (!chatClient){
+        return <div>Error, please try again.</div>
+    }
+
+    return (
+        <Chat client={chatClient} theme = 'str-chat__theme-dark'>
+            <section className='flex h-screen w-screen layout'>
+            <ChannelList />
+            <Channel>
+                <Window>
+                    <MessageList />
+                    <MessageInput />
+                </Window>
+                <Thread />
+            </Channel>
+            </section>
+        </Chat>
+    );
+
+
+
+
+
+}
