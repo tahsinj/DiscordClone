aaa2b62fbab5cf105781c0c555837f4f464c31a1
kmerchant1 <kaiden.merchant@gmail.com>
2024-12-01 13:43:23 -0800

    added header to the custom channel list side bar

--- discord-clone/components/ChannelList/CustomChannelList.tsx
+++ discord-clone/components/ChannelList/CustomChannelList.tsx
@@ -1,11 +1,12 @@
 import { useDiscordContext } from "@/context/DiscordContext";
+import ChannelListTopBar from "./TopBar/ChannelListTopBar";
 
 export default function CustomChannelList(): JSX.Element {
 
     const {server} = useDiscordContext();
 
     return(<div className='w-72 bg-medium-gray h-full flex flex-col items-start'>
-        <h2>CUSTOM CHANNEL LIST</h2>
-        <h2>{server?.name || 'Direct Messages'}</h2>
+        <ChannelListTopBar serverName = {server?.name || 'Direct Messages'}/>
+
     </div>);
 }
