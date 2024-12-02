be85894e50a14a9d13f71f4d5783ced41cd9c301
kmerchant1 <kaiden.merchant@gmail.com>
2024-12-01 18:43:59 -0800

    added a couple more icons

--- discord-clone/components/icons.tsx
+++ discord-clone/components/icons.tsx
@@ -239,3 +239,26 @@ export function ChevronDown({
       </svg>
     );
   }
+
+  export function PlusIcon({
+    className = 'h-5 w-5 text-gray-500',
+  }: IconProps): JSX.Element {
+    return (
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        fill='none'
+        viewBox='0 0 24 24'
+        strokeWidth={2}
+        stroke='currentColor'
+        className={className}
+      >
+        <path
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          d='M12 4.5v15m7.5-7.5h-15'
+        />
+      </svg>
+    );
+  }
+
+  
