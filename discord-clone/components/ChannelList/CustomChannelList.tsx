2e8ca5c9785ba5a1ca8cefd1c0029c1aae2392d4
kmerchant1 <kaiden.merchant@gmail.com>
2024-12-01 18:43:44 -0800

    shows currently selected server's channels in the customchannellist

--- discord-clone/components/ChannelList/CustomChannelList.tsx
+++ discord-clone/components/ChannelList/CustomChannelList.tsx
@@ -1,12 +1,23 @@
 import { useDiscordContext } from "@/context/DiscordContext";
 import ChannelListTopBar from "./TopBar/ChannelListTopBar";
+import CategoryItem from "./CategoryItem";
 
 export default function CustomChannelList(): JSX.Element {
 
-    const {server} = useDiscordContext();
+    const {server, channelsByCategories} = useDiscordContext();
 
     return(<div className='w-72 bg-medium-gray h-full flex flex-col items-start'>
         <ChannelListTopBar serverName = {server?.name || 'Direct Messages'}/>
-
-    </div>);
+            <div className='w-full'>
+                {Array.from(channelsByCategories.keys()).map((category, index) => (
+            <CategoryItem
+                key={`${category}-${index}`}
+                category={category}
+                serverName={server?.name || 'Direct Messages'}
+                channels={channelsByCategories.get(category) || []}
+            />
+        ))}
+            </div>
+        </div>
+        );
 }
