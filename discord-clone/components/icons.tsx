b3b04d7d02f1c2a35166f11301d585bfbf4d1c33
kmerchant1 <kaiden.merchant@gmail.com>
2024-12-01 19:39:35 -0800

    a couple more icons

--- discord-clone/components/icons.tsx
+++ discord-clone/components/icons.tsx
@@ -291,4 +291,23 @@ export function ChevronDown({
     );
   }
 
+  export function CloseMark({ className = 'w-5 h-5' }: IconProps): JSX.Element {
+    return (
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        fill='none'
+        viewBox='0 0 24 24'
+        strokeWidth={2}
+        stroke='currentColor'
+        className={className}
+      >
+        <path
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          d='M6 18 18 6M6 6l12 12'
+        />
+      </svg>
+    );
+  }
+
   
